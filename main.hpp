c8e8281e8f9cc6fba353757c1286e2db675b85fd
TealSmurf <jcoin707@gmail.com>
2024-03-15 23:34:35 -0700

    Created logic for finding first prime after begin and first prime before end First Run

--- main.hpp
+++ main.hpp
@@ -6,10 +6,44 @@ int getTwoValues(int begin, int end)
     do
     {
         cout << "Enter two integers: ";
+        cin >> begin >> end;
     } while (begin < end);
 }
 
-int getNextPrime(int)
+int getNextPrime(int begin)
 {
-    
+    int i, j;
+    for(int i = begin; ; i++)
+    {
+        for(j = 2; j <= (i/2); j++)
+        {
+            if(i % j == 0)
+            {
+                break;
+            }
+        }
+        if(j > 1/2)
+        {
+            int prime1 = i;
+        }
+    }
+}
+
+int getPrevPrime(int end)
+{
+    int i, j;
+    for(int j = end; ; i--)
+    {
+        for(j = 2; j <= (i/2); j++)
+        {
+            if(i % j == 0)
+            {
+                break;
+            }
+        }
+        if(j > 1/2)
+        {
+            int prime2 = i;
+        }
+    }
 }
